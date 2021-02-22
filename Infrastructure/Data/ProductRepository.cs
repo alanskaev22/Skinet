71443f5c85e09174787b88b91c0bade446d0cb28
Alan <alan.skaev22@gmail.com>
2021-02-21 22:23:34 -0500

    Implement Repository pattern

---
+++ Infrastructure/Data/ProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class ProductRepository : IProductRepository
+    {
+        public Task<Product> GetProductByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<Product>> GetProductsAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
