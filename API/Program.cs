e2d7bb6dfc19081a79ac073fac2dc22beabcf1a6
Alan <alan.skaev22@gmail.com>
2021-02-22 11:35:31 -0500

    Load initial data

--- API/Program.cs
+++ API/Program.cs
@@ -2,8 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Infrastructre.Data;
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -11,9 +15,30 @@ namespace API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            // Adding this so migration occures before app is started
+            using(var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+                try
+                {
+                    var context = services.GetRequiredService<StoreContext>();
+                    await context.Database.MigrateAsync();
+                    await StoreContextSeed.SeedAsync(context, loggerFactory);
+                }
+                catch (Exception ex)
+                {
+
+                    var logger = loggerFactory.CreateLogger<Program>();
+                    logger.LogError(ex, "An  Error Occured During Migration");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
---
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -0,0 +1,63 @@
+﻿using Core.Entities;
+using Infrastructre.Data;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class StoreContextSeed
+    {
+        public static async Task SeedAsync(StoreContext storeContext, ILoggerFactory loggerFactory)
+        {
+            try
+            {
+                if (!storeContext.ProductBrands.Any())
+                {
+                    var brandsData = File.ReadAllText("../Infrastructure/Data/SeedData/brands.json");
+                    var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandsData);
+
+                    foreach(var brand in brands)
+                    {
+                        storeContext.ProductBrands.Add(brand);
+                    }
+                    await storeContext.SaveChangesAsync();
+                }
+
+                if (!storeContext.ProductTypes.Any())
+                {
+                    var typesData = File.ReadAllText("../Infrastructure/Data/SeedData/types.json");
+                    var types = JsonSerializer.Deserialize<List<ProductType>>(typesData);
+
+                    foreach (var type in types)
+                    {
+                        storeContext.ProductTypes.Add(type);
+                    }
+                    await storeContext.SaveChangesAsync();
+                }
+
+                if (!storeContext.Products.Any())
+                {
+                    var productsData = File.ReadAllText("../Infrastructure/Data/SeedData/products.json");
+                    var products = JsonSerializer.Deserialize<List<Product>>(productsData);
+
+                    foreach (var product in products)
+                    {
+                        storeContext.Products.Add(product);
+                    }
+                    await storeContext.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                var logger = loggerFactory.CreateLogger<StoreContextSeed>();
+                logger.LogError(ex.Message);
+            }
+        }
+    }
+}
